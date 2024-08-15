7264b7117a631beef3ffa332a99703c64264ff1d
fatemeh <babaei.f1996@gmail.com>
2024-08-15 15:50:36 +0330

    feat: slider hero

--- my-app/components/page/landing/HeroSlider.tsx
+++ my-app/components/page/landing/HeroSlider.tsx
@@ -5,9 +5,11 @@ import hero2 from "@/public/images/landing/hero/crs-14.jpg"
 import hero3 from "@/public/images/landing/hero/crs-15.jpg"
 import hero4 from "@/public/images/landing/hero/static-22.jpg"
 import hero5 from "@/public/images/landing/hero/static-23.jpg"
-import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
+import { Navigation, Pagination, Scrollbar, A11y, Autoplay } from 'swiper/modules';
 // Import Swiper React components
 import { Swiper, SwiperSlide } from 'swiper/react';
+
+import 'swiper/css/autoplay';
 
 // Import Swiper styles
 import 'swiper/css';
@@ -19,63 +21,72 @@ const HeroSlider = () => {
     return (
         <div>
             <Swiper
-                modules={[Navigation, Pagination, Scrollbar, A11y]}
+                modules={[Navigation, Pagination, Scrollbar, A11y, Autoplay]}
                 spaceBetween={50}
                 slidesPerView={1}
                 navigation
                 loop={true}
-                autoplay={true}
+                autoplay={{ delay: 1000 }}
                 pagination={{ clickable: true }}
                 onSwiper={(swiper) => console.log(swiper)}
                 onSlideChange={() => console.log('slide change')}
             >
                 <SwiperSlide>
-                    <Image
-                        className='aspect-[1.2] rounded-2xl object-cover'
-                        src={hero1}
-                        width={800}
-                        height={800}
-                        alt="Bitski"
-                    />
+                    <div className="group overflow-hidden rounded-2xl transition ease-in-out delay-750">
+                        <Image
+                            className='aspect-[1.2] transition max-h-[600px] ease-in-out delay-750 hover:scale-[1.1] rounded-2xl object-cover'
+                            src={hero1}
+                            width={800}
+                            height={800}
+                            alt="Bitski"
+                        />
+                    </div>
                 </SwiperSlide>
                 <SwiperSlide>
-                    <Image
-                        className='aspect-[1.2] rounded-2xl object-cover'
-                        src={hero2}
-                        width={800}
-                        height={800}
-                        alt="Bitski"
-                    />
+                    <div className="group overflow-hidden rounded-2xl transition ease-in-out delay-750">
+                        <Image
+                            className='aspect-[1.2] transition max-h-[600px] ease-in-out delay-750 hover:scale-[1.1] rounded-2xl object-cover'
+                            src={hero2}
+                            width={800}
+                            height={800}
+                            alt="Bitski"
+                        />
+                    </div>
                 </SwiperSlide>
                 <SwiperSlide>
-                    <Image
-                        className='aspect-[1.2] rounded-2xl object-cover'
-                        src={hero3}
-                        width={800}
-                        height={800}
-                        alt="Bitski"
-                    />
+                    <div className="group overflow-hidden rounded-2xl transition ease-in-out delay-750">
+                        <Image
+                            className='aspect-[1.2] hover:scale-[1.1] max-h-[600px] transition ease-in-out delay-750 rounded-2xl object-cover group-hover:scale-[1.1]'
+                            src={hero3}
+                            width={800}
+                            height={800}
+                            alt="Bitski"
+                        />
+                    </div>
                 </SwiperSlide>
                 <SwiperSlide>
-                    <Image
-                        className='aspect-[1.2] rounded-2xl object-cover'
-                        src={hero4}
-                        width={800}
-                        height={800}
-                        alt="Bitski"
-                    />
+                    <div className='group overflow-hidden rounded-2xl transition ease-in-out delay-750'>
+                        <Image
+                            className='aspect-[1.2] rounded-2xl  max-h-[600px] transition ease-in-out delay-750 over:scale-[1.1] object-cover group-hover:scale-[1.1]'
+                            src={hero4}
+                            width={800}
+                            height={800}
+                            alt="Bitski"
+                        />
+                    </div>
                 </SwiperSlide>
                 <SwiperSlide>
-                    <Image
-                        className='aspect-[1.2] rounded-2xl object-cover'
-                        src={hero5}
-                        width={800}
-                        height={800}
-                        alt="Bitski"
-                    />
-                </SwiperSlide>
+                    <div className='group overflow-hidden rounded-2xl transition ease-in-out delay-150'>
+                        <Image
+                            className='aspect-[1.2] max-h-[600px] transition ease-in-out delay-150 hover:scale-[1.1] rounded-2xl object-cover'
+                            src={hero5}
+                            width={800}
+                            height={800}
+                            alt="Bitski"
+                        />
+                    </div>
 
-
+                </SwiperSlide>
             </Swiper>
         </div>
     )
