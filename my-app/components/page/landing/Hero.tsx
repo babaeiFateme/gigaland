fcb0a53d2e6119d5bf2cf0783a4e6debb28d39ca
fatemeh <babaei.f1996@gmail.com>
2024-08-15 21:14:48 +0330

    style: fix style for hero section

--- my-app/components/page/landing/Hero.tsx
+++ my-app/components/page/landing/Hero.tsx
@@ -7,18 +7,20 @@ const Hero = () => {
                 <div className="py-[30px] px-[50px]">
                     <div>
                         <h1 className="text-2xl md:text-4xl font-bold">
-                            Explore, collect, and sell extraordinary
+                            <span>
+                                Explore, collect, and sell extraordinary
+                            </span>
+                            <span className="ml-2 text-2xl md:text-4xl font-bold text-gradient">
+                                NFTs
+                            </span>
                         </h1>
-                        <span className="text-2xl md:text-4xl font-bold text-gradient">
-                            NFTs
-                        </span>
                     </div>
                     <div className="text-gray-500 font-semibold text-xl max-w-[400px] my-8">
                         Welcome to the future, you can buy and sell awesome artworks form here. The world largest digital marketplace for non-fungible tokens.
                     </div>
                     <div className="grid grid-cols-2 gap-4 max-w-[300px] mt-5">
-                        <button type="button" className="text-white bg-primary-500 py-3 px-10 rounded-lg">Explore</button>
-                        <button type="button" className="text-white bg-primary-500 py-3 px-10 rounded-lg">Sell</button>
+                        <button type="button" className="btn-gradient font-bold text-white bg-primary-500 py-3 px-10 rounded-lg">Explore</button>
+                        <button type="button" className="text-white font-bold text-primary-400 bg-primary-500 py-3 px-10 rounded-lg">Sell</button>
                     </div>
                 </div>
                 <HeroSlider />
